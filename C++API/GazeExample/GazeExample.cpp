5dfda8468f29616cef2720b54e108b592e8ef9e6
Zhenyi He <snowymohehe@gmail.com>
2020-09-09 06:32:30 -0400

    :star2: adapt to new windows. now support using command to specify width and height

--- C++API/GazeExample/GazeExample.cpp
+++ C++API/GazeExample/GazeExample.cpp
@@ -2,26 +2,42 @@
 //
 
 #include "tcpConnection.h"
-
+#include <sstream>     /* strtol */
 #include <iostream>
 #include <interaction_lib/InteractionLib.h>
 #include <interaction_lib/misc/InteractionLibPtr.h>
 
+
 TCPMode tcpMode = TCPMode::Server;
 
-int main()
+bool enableNetwork = true;
+
+int main(int argc, char *argv[])
 {
-	tcpSetup(tcpMode);
-	if (tcpMode == TCPMode::Server)
-		tcpListen();
+	if (enableNetwork) {
+		tcpSetup(tcpMode);
+		if (tcpMode == TCPMode::Server)
+			tcpListen();
+	}	
 
     // create the interaction library
     IL::UniqueInteractionLibPtr intlib(IL::CreateInteractionLib(IL::FieldOfUse::Interactive));
     // assume single screen with size 2560x1440 and use full screen (not window local) coordinates
-    constexpr float width = 1920.0f;// 2560.0f;
-    constexpr float height = 1200.0f;// 1440.0f;
+	float defaultWidth = 1920;
+	float defaultHeight = 1080;
+	if (argc == 3) {
+		// argv[1] should be width and argv[2] should be height
+		std::stringstream str;
+		str << argv[1];
+		str >> defaultWidth;
+		std::stringstream str2;
+		str << argv[2];
+		str >> defaultHeight;
+	}
+	constexpr float width = 1920.0f;// 2560.0f;
+	constexpr float height = 1080;// 1440.0f;
     constexpr float offset = 0.0f;
-    intlib->CoordinateTransformAddOrUpdateDisplayArea(width, height);
+    intlib->CoordinateTransformAddOrUpdateDisplayArea(defaultWidth, defaultHeight);
     intlib->CoordinateTransformSetOriginOffset(offset, offset);
     // subscribe to gaze point data; print data to stdout when called
     intlib->SubscribeGazePointData([](IL::GazePointData evt, void* context)
@@ -38,7 +54,8 @@ int main()
         memcpy_s(buffer, 4, &(evt.x), 4);
         memcpy_s(buffer+4, 4, &(evt.y), 4);*/
         std::string gazeMsg = evt.validity == IL::Validity::Valid ? std::to_string(evt.x) + " " + std::to_string(evt.y) : "";
-        tcpSendMsg(tcpMode, gazeMsg);
+		if(enableNetwork)
+			tcpSendMsg(tcpMode, gazeMsg);
     }, nullptr);
     std::cout << "Starting interaction library update loop.\n";
 
